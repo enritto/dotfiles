e2d68ab77a5f24d2c39a13261fab17a2a63dbfb9
enritto <enritto@debian>
2021-10-20 22:41:07 +0500

    added a togglestruts hotkey on mod b and xmobar on lower layer instead of just killing xmobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,14 +8,17 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Renamed
 myWorkspaces = ["dev", "www", "sys", "chat", "misc", "doc", "mus", "vid", "gfx"]
 
-myLayout = smartBorders . avoidStruts $ spacing 8 $ Tall 1 (3/100) (1/2) ||| Full
+myLayout = renamed [CutWordsLeft 1] $ smartBorders . avoidStruts $ spacing 8 $ tiled ||| full
   where
-    tiled    = Tall nmaster delta ratio
+    tiled    = renamed [Replace "master & stack"] (Tall nmaster delta ratio)
     nmaster  = 1      -- Default number of windows in the master pane
     ratio    = 1/2    -- Default proportion of screen occupied by master pane
     delta    = 3/100  -- Percent of screen to increment by when resizing panes
+    full = renamed [Replace "monocle"] (Full)	
+	
 main :: IO ()
 main = do 
 xmproc <- spawnPipe "xmobar"
@@ -43,5 +46,5 @@ xmonad $ docks def
 	, ("M-S-<Return>"  , spawn "rofi -show run")
 	, ("M-<Return>"	, spawn "kitty")
 	, ("M-f" , spawn ("kitty -e ranger"))
-	, ("M-b" , spawn "killall xmobar")
+	, ("M-b" , sendMessage ToggleStruts)
      ]
